99567400a559b86bb04fa9db09cf9c6c1f9a1764
best-and-usa-ny <best.and.usa.ny@gmail.com>
2025-11-15 00:55:49 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ export const config = {
 };
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://analyzer.лучшее-и-люди.рф');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
